24676472c8dc946f4ac1bed7d8ccf9ab16d2318c
Nicolas Jourdan <Nicolas.Jourdan@me.com>
2023-11-15 14:14:10 +0100

    Update

--- src/interq_cip_qhs/config.py
+++ src/interq_cip_qhs/config.py
@@ -3,4 +3,4 @@ class Config:
         self.DATASET_PATH = "/Users/nicolasjourdan/projects/temp/interq_cip_qhs/src/interq_cip_qhs/notebooks/example_data/"
         self.cid = "2WwhkHtuCLBXdnv3M9BN5jGE2wToKuiQmZ6YRwH8BeKb"
         self.pwd = "interq"
-        self.model = "demo_process_milling_data_11"
+        self.model = "demo_process_milling_data_12"
